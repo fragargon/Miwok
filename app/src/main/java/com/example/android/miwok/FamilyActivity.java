b1e4bf26203e0c29cd76ba63a1ec5f0ab361cdd6
fragargon <fragargon@gmail.com>
2018-02-07 23:44:02 +0100

    refactor category activity to use fragment

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -1,147 +1,16 @@
 package com.example.android.miwok;
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 public class FamilyActivity extends AppCompatActivity {
-
-    /** Handles audio focus when playing a sound files*/
-    private AudioManager mAudioManager;
-
-    /** Handles playback off all the sound files */
-    private MediaPlayer mMediaPlayer;
-
-    /**
-     * This listener gets triggered whenever the audio focus changes
-     * (i.e. we gain or lose audio focus because of another app or device)
-     */
-    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
-        @Override
-        public void onAudioFocusChange(int focusChange) {
-            if(focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
-                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
-                mMediaPlayer.pause();
-                mMediaPlayer.seekTo(0);
-            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
-                mMediaPlayer.start();
-            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
-                releaseMediaPlayer();
-            }
-        }
-    };
-
-    /**
-     * This listener gets triggered when the {@link MediaPlayer} has completed playing audio file
-     */
-
-    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mp) {
-            // Now that the sound file has finished playing, release the media resources
-            releaseMediaPlayer();
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new FamilyFragment())
+                .commit();
 
-        // Create and setup the {@link AudioManager} to request audio focus
-        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-        // Create an arrayList of words
-        final ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("father", "әpәv", R.drawable.family_father,R.raw.family_father));
-        words.add(new Word("mother", "әṭa", R.drawable.family_mother,R.raw.family_mother));
-        words.add(new Word("son", "angsi", R.drawable.family_son,R.raw.family_son));
-        words.add(new Word("daughter", "tune", R.drawable.family_daughter,R.raw.family_daughter));
-        words.add(new Word("older brother", "taachi", R.drawable.family_older_brother,R.raw.family_older_brother));
-        words.add(new Word("younger brother", "chalitti", R.drawable.family_younger_brother,R.raw.family_younger_brother));
-        words.add(new Word("older sister", "teṭe", R.drawable.family_older_sister,R.raw.family_older_sister));
-        words.add(new Word("younger sister", "kolliti", R.drawable.family_younger_sister,R.raw.family_younger_sister));
-        words.add(new Word("grandmother", "ama", R.drawable.family_grandmother,R.raw.family_grandmother));
-        words.add(new Word("grandfather", "paapa", R.drawable.family_grandfather,R.raw.family_grandfather));
-
-        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
-        // adapter knows how to create layouts for each item in the list
-        WordAdapter adapter = new WordAdapter(this, words, R.color.category_family);
-
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_list.xml file.
-        ListView listView = (ListView) findViewById(R.id.list);
-
-        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
-        // {@link ListView} will display list items for each word in the list of words.
-        listView.setAdapter(adapter);
-
-        // Bind the abstract method to the ListView and gives parameters to its interface
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Release the media player if it currently exists because we are about
-                // to play a different sound file
-                releaseMediaPlayer();
-
-                // get the {@link Word} object at the given position the user clicked on
-                Word word = words.get(position);
-                Log.v("FamilyActivity", "Current word: " + word);
-
-                // Request audio focus so in order to play the audio file
-                // The app needs to play a short audio file
-                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener, AudioManager.STREAM_MUSIC
-                        ,AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                    // We have focus now
-                    // Create and setup the {@link MediaPlayer} for the audio resource associated with the current word
-                    mMediaPlayer = MediaPlayer.create(FamilyActivity.this, word.getAudioResourceID());
-                    // Start the audio file
-                    mMediaPlayer.start();
-
-                    // Setup a listener on the media player, so that we can stop and release the
-                    // media player once the sound has finished playing
-                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
-
-                }
-            }
-        });
-
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // When the activity is stopped, release the media player resources because
-        // we don't playing anymore sounds
-        releaseMediaPlayer();
-    }
-
-    /**
-     * Clean up the media player by releasing its resources
-     */
-
-    private void releaseMediaPlayer() {
-        // If the media player is not null, the it may be currently playing a sound.
-        if (mMediaPlayer != null) {
-            // Regardless of the current state of the media player, release its resource
-            // because we no longer need it
-            mMediaPlayer.release();
-            // set the media player back to null. Four our code, we have decided that
-            // setting the media player to null is an easy way to tell that the media player
-            // is not configured to play an audio file at the moment
-            mMediaPlayer = null;
-            // regardless whether or not we were granted audio focus, abandon it.
-            // unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
-            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
-        }
     }
 }
