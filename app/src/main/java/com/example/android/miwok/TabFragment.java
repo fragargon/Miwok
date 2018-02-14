f1d087f925976dbc6316c0a33c4f97aabd0a86d1
fragargon <fragargon@gmail.com>
2018-02-14 18:06:17 +0100

    refactor category activity to use fragment

---
+++ app/src/main/java/com/example/android/miwok/TabFragment.java
@@ -0,0 +1,42 @@
+package com.example.android.miwok;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Provides the appropriate {@link TabFragment} for a view pager
+ */
+
+public class TabFragment extends Fragment{
+    public static final String ARG_PAGE = "ARG_PAGE";
+
+    private int mPage;
+
+    public static TabFragment newInstance(int page) {
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        TabFragment fragment = new TabFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPage = getArguments().getInt(ARG_PAGE);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_tab, container, false);
+        TextView tv = (TextView) v;
+        tv.setText("Fragment #" + mPage);
+        return v;
+    }
+}
